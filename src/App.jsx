12b7f8174e133c8b4d2db60e8ce324fb913ba69e
camilito2005 <marrugobarrioscamilo2005@gmail.com>
2025-09-11 19:59:59 -0500

    feat: añadi los componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Layout } from "./componentes/layouts.jsx";
+import { Inicio } from "./paginas/inicio.jsx";
+// import { Experiencias } from "./paginas/experiencias.jsx";
+// import { Artesanias } from "./paginas/artesanias.jsx";
+// import { Comunidad } from "./paginas/comunidad.jsx";
+import { Admin } from "./paginas/admin.jsx";
+import { Reservas } from "./paginas/reservas.jsx";
+import { Marketplace } from "./paginas/catalogo.jsx";
+import { Perfil } from "./paginas/perfil.jsx";
+import { Registro } from "./paginas/registro.jsx";
+import { Login } from "./paginas/login.jsx";
+import { Contactanos } from "./paginas/contactanos.jsx";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Router>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Inicio />} />
+          <Route path="/reservas" element={<Reservas />} />
+          <Route path="/marketplace" element={<Marketplace />} />
+          <Route path="/perfil" element={<Perfil />} />
+          <Route path="/admin" element={<Admin />} />
+          <Route path="/contacto" element={<Contactanos />} />
+          <Route path="/registro" element={<Registro />} />
+          <Route path="/login" element={<Login />} />
+
+          
+        </Routes>
+      </Layout>
+    </Router>
+  );
 }
 
-export default App
+export default App;
