d5af1a0fb6f17b0d1d29ee15bd86235a89316cd0
camilito2005 <marrugobarrioscamilo2005@gmail.com>
2025-10-21 23:34:22 -0500

    ,

--- Backend/Modelo/Usuariosmodel.php
+++
@@ -1,6 +0,0 @@
-<?php
-function UsuariosModel($conexion, $nombre, $identificacion, $telefono, $direccion, $email, $contraseña_hash, $cargo) {
-    // inserto los parametros en la base de datos
-    $sql = "INSERT INTO usuarios (nombre, email, contraseña, cargo ,telefono, direccion, , , ,identificacion) VALUES (?, ?, ?, ?, ?, ?, ?)";
-}
-?>
