a6c3132b62823f7388c282f4f30cdb128bd6e248
camilito2005 <marrugobarrioscamilo2005@gmail.com>
2025-10-21 22:53:55 -0500

    feat: agregue path

---
+++ Backend/app.mjs
@@ -0,0 +1,26 @@
+import express from 'express';// esta línea importa el paquete express
+import dotenv from 'dotenv';// esta línea importa el paquete dotenv
+import cors from 'cors'; // esta línea importa el paquete cors lo cual permite compartir recursos entre diferentes orígenes
+import path from "path";
+import { fileURLToPath } from "url";
+
+import CargosRoutes from './src/Rutas/Cargos.routes.mjs'; // esta línea importa las rutas de usuarios
+import UsuariosRoutes from './src/Rutas/Usuarios.routes.mjs'; // esta línea importa las rutas de usuarios
+
+dotenv.config();// esta línea carga las variables de entorno desde el archivo .env
+const app = express();// esta línea crea una instancia de una aplicación express
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Servir la carpeta Recursos públicamente
+app.use("/Recursos", express.static(path.join(__dirname, "src","/Recursos")));
+app.use(cors());
+
+app.use('/api/cargos', CargosRoutes);// esta línea monta las rutas de usuarios en la ruta /cargos
+app.use('/api/usuarios', UsuariosRoutes);// esta línea monta las rutas de usuarios en la ruta /usuarios
+
+app.use(express.json());// esta línea permite que la aplicación pueda interpretar solicitudes con cuerpo en formato JSON
+
+app.listen(process.env.PORT || 3000, () => // esta línea inicia el servidor en el puerto especificado en las variables de entorno o en el puerto 3000 si no está especificado
+  console.log(`Servidor corriendo en http://localhost:${process.env.PORT}`)
+);
