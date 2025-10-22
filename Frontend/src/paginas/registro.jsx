d5ee8e0e06d2e9f98ec97f4d53fd045ad5604a84
camilito2005 <marrugobarrioscamilo2005@gmail.com>
2025-10-21 22:31:30 -0500

    feat: añadi el modal

--- Frontend/src/paginas/registro.jsx
+++ Frontend/src/paginas/registro.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { useEffect, useState } from "react";
 import { Getdata, PostFormData } from "../servicios/Apis.js";
-import { Link } from "react-router-dom";
 import { Modal } from "../componentes/Modal.jsx";
 
 import "../css/registro.css";
