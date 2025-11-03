b9dc2e6f6a0189ab728309e2171181f2bacc4ed1
camilito2005 <marrugobarrioscamilo2005@gmail.com>
2025-11-03 13:31:39 -0500

    feat: añadi la ruta para el login

--- Backend/src/Rutas/Usuarios.routes.mjs
+++ Backend/src/Rutas/Usuarios.routes.mjs
@@ -2,11 +2,14 @@ import { Router } from "express";
 import { upload } from "../Middlewares/upload.mjs";
 import { RegistrarUsuarios } from "../Controladores/Usuarios.controlador.mjs";
 import { ListarUsuarios } from "../Controladores/Usuarios.controlador.mjs";
+import { AutenticarUsuario } from "../Controladores/Usuarios.controlador.mjs";
 
 const router = Router(); // esta variable nos permite crear rutas
 
 // router.post("/Registrarusuarios",RegistrarUsuarios);
 router.post("/Registrarusuarios", upload.single("foto"), RegistrarUsuarios);
 router.get("/Listarusuarios", ListarUsuarios);
+router.post("/Login", AutenticarUsuario); // nueva ruta para autenticacion
+
 
 export default router; // exporta las rutas para usarlas en otros archivos  
