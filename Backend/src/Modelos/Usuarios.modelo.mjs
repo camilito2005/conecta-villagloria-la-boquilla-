2d375bf900d4b4b6cc07e74c511c6943ee658ddb
camilito2005 <marrugobarrioscamilo2005@gmail.com>
2025-11-03 13:38:44 -0500

    feat: en el modelo agregue las respectivas funciones para autenticar el ususario, la cual esta incompleta

--- Backend/src/Modelos/Usuarios.modelo.mjs
+++ Backend/src/Modelos/Usuarios.modelo.mjs
@@ -33,7 +33,21 @@ export async function RegistrarUsuario(NuevoUsuario) {
   return { id: rows[0].id, ...NuevoUsuario };// devuelvo el id del nuevo usuario junto con los demás datos
 }
 export async function ObtenerUsuarios() {
-  const consulta = "SELECT u.id_usuario,u.nombre,u.email, u.contraseña,c.cargo AS descripcion_cargo,u.imagen_url,u.telefono,u.direccion,u.identificacion,u.estado FROM  public.usuarios u INNER JOIN public.cargo c ON u.id_cargo = c.id_cargo";
+  const consulta = "SELECT u.id_usuario,u.nombre,u.email,c.cargo AS descripcion_cargo,u.imagen_url,u.telefono,u.direccion,u.identificacion,u.estado FROM  public.usuarios u INNER JOIN public.cargo c ON u.id_cargo = c.id_cargo";
   const { rows } = await pool.query(consulta);
   return rows;
+}
+export async function BuscarUsuarioPorEmail(email) {
+  const consulta = "SELECT * FROM usuarios WHERE email = $1";
+  const { rows } = await pool.query(consulta, [email]);
+  console.log("Usuario encontrado por email:", rows[0]);
+  if (rows === undefined || rows.length === 0) {
+    return null;
+  }
+  return rows[0];
+}
+export async function Comparacontraseñas(contraseña,contraseñahased){
+  const consulta = "SELECT * FROM usuarios where contraseña = $1";
+  const {Filas} = await pool.query(consulta, [contraseña,contraseñahased]);
+  console.log("Resultado de la comparacion de contraseñas", Filas[0]);
 }
