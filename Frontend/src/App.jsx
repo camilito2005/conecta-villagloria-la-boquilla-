e554bd6bec2472f76aecb8cf771780e006f1de2d
camilito2005 <marrugobarrioscamilo2005@gmail.com>
2025-11-18 11:52:01 -0500

    feat: agregue al componente nuevas rutas

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,13 +2,15 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Layout } from "./componentes/layouts.jsx";
 import { Inicio } from "./paginas/inicio.jsx";
 import { Admin } from "./paginas/admin.jsx";
-import { Reservas } from "./paginas/reservas.jsx";
+import { ToursDisponibles } from "./paginas/reservas.jsx";
 import { Marketplace } from "./paginas/catalogo.jsx";
 import { Perfil } from "./paginas/perfil.jsx";
 import { Registro } from "./paginas/registro.jsx";
 import { Login } from "./paginas/login.jsx";
 import { Contactanos } from "./paginas/contactanos.jsx";
 import { Usuarios } from "./paginas/Usuarios.jsx";
+import { Tablas } from "./paginas/tablas.jsx";
+import { Agg_horarios } from "./paginas/Horarios.jsx";
 import "./App.css";
 
 function App() {
@@ -17,7 +19,7 @@ function App() {
       <Layout>
         <Routes>
           <Route path="/" element={<Inicio />} />
-          <Route path="/reservas" element={<Reservas />} />
+          <Route path="/reservas" element={<ToursDisponibles />} />
           <Route path="/marketplace" element={<Marketplace />} />
           <Route path="/perfil" element={<Perfil />} />
           <Route path="/admin" element={<Admin />} />
@@ -25,6 +27,8 @@ function App() {
           <Route path="/registro" element={<Registro />} />
           <Route path="/login" element={<Login />} />
           <Route path="/usuarios" element={<Usuarios />} />
+          <Route path="/ejercicio" element={<Tablas />} />
+          <Route path="/agg_horarios" element={<Agg_horarios />} />
           
         </Routes>
       </Layout>
