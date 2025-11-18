e60c52954e1f4a3d82b895fe2b01e223e3cdcf24
camilito2005 <marrugobarrioscamilo2005@gmail.com>
2025-11-18 12:16:35 -0500

    feat: cree una validacion pero no sirve de momento

--- Frontend/src/componentes/Modal.jsx
+++ Frontend/src/componentes/Modal.jsx
@@ -1,10 +1,17 @@
 ///modal para mostrar mensajes de errores o resultados de las llamas a la api
 import "../css/modal.css";
 export function Modal({ title, message, type, onClose }) {
+  let clases = "";
+  if (type == "exitoso") {
+     clases = "modal-title-exit"
+  }
+  else if(type == "error"){
+     clases = "modal-title-error"
+  }
   return (
     <div className="modal-overlay">
       <div className={`modal ${type}`}>
-        <h2 className="modal-title">{title}</h2>
+        <h2 className={`${clases}`}>{title}</h2>
         <p className="modal-message">{message}</p>
         <button className="modal-close-button" onClick={onClose}>Cerrar</button>
       </div>
