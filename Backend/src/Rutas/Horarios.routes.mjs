31750dd06045ae767cef0ff72e507fb771984d7b
camilito2005 <marrugobarrioscamilo2005@gmail.com>
2025-11-20 14:32:14 -0500

    feat:agregue ls respectivas rutas para las funciones de los horarios

--- Backend/src/Rutas/Horarios.routes.mjs
+++ Backend/src/Rutas/Horarios.routes.mjs
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { IngresarHorario, ListarHorarios } from "../Controladores/Horarios.controlador.mjs";
+import { VerificarToken } from "../Middlewares/Auth.mjs";
 
 const router = Router();
 
-router.post("/agg_horarios", IngresarHorario);
-router.get("/listar_horarios", ListarHorarios);
+router.post("/agg_horarios",VerificarToken, IngresarHorario);
+router.get("/listar_horarios",VerificarToken, ListarHorarios);
 
 export default router; // exporta las rutas para usarlas en otros archivos
