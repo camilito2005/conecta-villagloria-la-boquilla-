9f86a65b7c62a8ba44cbd0c6bd15e2799e8357fd
camilito2005 <marrugobarrioscamilo2005@gmail.com>
2025-11-20 13:58:41 -0500

    fix: corregi la funcion

--- Frontend/src/servicios/Auth.js
+++ Frontend/src/servicios/Auth.js
@@ -3,12 +3,12 @@
 import { useEffect } from "react";
 import { Postdata } from "../servicios/Apis.js";
 
-export const VerificarSesion = ({ setUsuario, setModalData, navigate }) => {
+export function  useVerificarSesion ({ setUsuario, setModalData, navigate }) {
   useEffect(() => {
     const verificar = async () => {
       const result = await Postdata("usuarios/verificar", { enviarJson: true });
 
-      if (result.showModal && result.modal) {
+      if (result.showModal && result.modal && result.autenticado === false) {
         setModalData({
           title: result.modal.title,
           message: result.modal.message,
