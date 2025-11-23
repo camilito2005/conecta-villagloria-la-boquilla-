22eb8a2f37c5a51062ba851d65b36d650a79af72
camilito2005 <marrugobarrioscamilo2005@gmail.com>
2025-11-23 15:46:48 -0500

    feat: añadi las funciones para inactivar, obtener y restaurar usuarios

--- Backend/src/Modelos/Usuarios.modelo.mjs
+++ Backend/src/Modelos/Usuarios.modelo.mjs
@@ -1,11 +1,5 @@
 import pool from "../Configuracion/Conexion.mjs";
 
-export async function CompararIdentificacion(identificacion) {
-  const consulta = "SELECT * FROM usuarios WHERE identificacion = $1";
-  const { rows } = await pool.query(consulta, [identificacion]);
-  return rows.length > 0;
-
-}
 export async function CompararEmail(email) {
   const consulta = "SELECT * FROM usuarios WHERE email = $1";
   const { rows } = await pool.query(consulta, [email]);
@@ -14,8 +8,8 @@ export async function CompararEmail(email) {
 
 export async function RegistrarUsuario(NuevoUsuario) {
   const consulta = `
-    INSERT INTO usuarios (nombre, email, contraseña, id_cargo, imagen_url, telefono, direccion, identificacion)
-    VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
+    INSERT INTO usuarios (nombre, email, contraseña, id_cargo, imagen_url, telefono)
+    VALUES ($1, $2, $3, $4, $5, $6)
     RETURNING id_usuario AS id
   `;
   const Valores = [
@@ -25,15 +19,13 @@ export async function RegistrarUsuario(NuevoUsuario) {
     NuevoUsuario.cargo,
     NuevoUsuario.foto,
     NuevoUsuario.telefono,
-    NuevoUsuario.direccion,
-    NuevoUsuario.identificacion
   ];
   const { rows } = await pool.query(consulta, Valores);
   
   return { id: rows[0].id, ...NuevoUsuario };// devuelvo el id del nuevo usuario junto con los demás datos
 }
 export async function ObtenerUsuarios() {
-  const consulta = "SELECT u.id_usuario,u.nombre,u.email,c.cargo AS descripcion_cargo,u.imagen_url,u.telefono,u.direccion,u.identificacion,u.estado FROM  public.usuarios u INNER JOIN public.cargo c ON u.id_cargo = c.id_cargo WHERE u.estado = 'activo' ";
+  const consulta = "SELECT u.id_usuario,u.nombre,u.email,c.cargo AS descripcion_cargo,u.imagen_url,u.telefono,u.direccion,u.estado FROM  public.usuarios u INNER JOIN public.cargo c ON u.id_cargo = c.id_cargo WHERE u.estado = 'activo' ";
   const { rows } = await pool.query(consulta);
   return rows;
 }
@@ -77,16 +69,6 @@ export async function Actualizar_Perfil_Admin(usuarioId, DatosActualizados) {
   }
 }
 
-export async function EliminarUsuario(usuarioId) {
-  try {
-    const consulta = `UPDATE usuarios set estado = 'inactivo' WHERE id_usuario = $1 RETURNING id_usuario, nombre, email, estado`;
-    const { rows } = await pool.query(consulta, [usuarioId]);
-    return rows[0]; // Devuelve el usuario eliminado (inactivado)
-  } catch (error) {
-    throw error;
-  }
-}
-
 export async function BuscarUsuarioPorId(usuarioId) {
   const consulta = `
     SELECT u.id_usuario, u.nombre, u.email, u.id_cargo, u.imagen_url, u.telefono, u.direccion, u.estado, 
@@ -105,4 +87,29 @@ export async function Comparacontraseñas(contraseña,contraseñahased){
   const consulta = "SELECT * FROM usuarios where contraseña = $1";
   const {Filas} = await pool.query(consulta, [contraseña,contraseñahased]);
   // console.log("Resultado de la comparacion de contraseñas", Filas[0]);
+}
+
+export async function Inactivarusuarios(usuarioId) {
+  try {
+    const consulta = `UPDATE usuarios set estado = 'inactivo' WHERE id_usuario = $1 RETURNING id_usuario, nombre, email, estado`;
+    const { rows } = await pool.query(consulta, [usuarioId]);
+    return rows[0]; // Devuelve el usuario eliminado (inactivado)
+  } catch (error) {
+    throw error;
+  }
+}
+
+export async function ObtenerUsuariosInactivos() {
+  const consulta = "SELECT u.id_usuario,u.nombre,u.email,c.cargo AS descripcion_cargo,u.imagen_url,u.telefono,u.direccion,u.estado FROM  public.usuarios u INNER JOIN public.cargo c ON u.id_cargo = c.id_cargo WHERE u.estado = 'inactivo' ";
+  const { rows } = await pool.query(consulta);
+  return rows;
+}
+export async function RestaurarUsuarios(id) {
+  try {
+    const consulta = `UPDATE usuarios set estado = 'activo' WHERE id_usuario = $1 RETURNING id_usuario, nombre, email, estado`;
+    const { rows } = await pool.query(consulta, [id]);
+    return rows[0]; // Devuelve el usuario restaurado (activado)
+  } catch (error) {
+    throw error;
+  }
 }
