700077ff0e6607ed3348a68c34ad624ad1884357
camilito2005 <marrugobarrioscamilo2005@gmail.com>
2025-11-23 16:27:09 -0500

    feat: añadi las rutas para inactivar y restaurar usuarios

--- Backend/src/Rutas/Usuarios.routes.mjs
+++ Backend/src/Rutas/Usuarios.routes.mjs
@@ -7,7 +7,9 @@ import { ActualizarPerfilAdmin } from "../Controladores/Usuarios.controlador.mjs
 import { AutenticarUsuario } from "../Controladores/Usuarios.controlador.mjs";
 import { CerrarSesion } from "../Controladores/Usuarios.controlador.mjs";   
 import { VerificarToken } from "../Middlewares/Auth.mjs";
-import { Eliminarusuario } from "../Controladores/Usuarios.controlador.mjs";
+import { Inactivarusuario } from "../Controladores/Usuarios.controlador.mjs";
+import { UsuariosInactivos } from "../Controladores/Usuarios.controlador.mjs";
+import { RestaurarUsuario } from "../Controladores/Usuarios.controlador.mjs";
 
 const router = Router(); // esta variable nos permite crear rutas
 
@@ -16,10 +18,12 @@ router.post("/Registrarusuarios", upload.single("foto"), RegistrarUsuarios);
 router.get("/Listarusuarios",VerificarToken, ListarUsuarios);
 router.get("/perfil_admin/:usuarioId",VerificarToken, PerfilAdmin);
 router.put("/actualizar_perfil_admin/:usuarioId",VerificarToken, ActualizarPerfilAdmin);
-router.post("/Eliminarusuario/:usuarioId",VerificarToken,Eliminarusuario);
+router.post("/Inactivarusuario/:usuarioId",VerificarToken,Inactivarusuario);
 router.post("/Login", AutenticarUsuario); // nueva ruta para autenticacion
+router.get("/inactivos", VerificarToken, UsuariosInactivos);
 router.post("/verificar", VerificarToken);
 router.post("/logout", CerrarSesion);
+router.post("/restaurar/:id",VerificarToken, RestaurarUsuario);
 
 
 export default router; // exporta las rutas para usarlas en otros archivos  
