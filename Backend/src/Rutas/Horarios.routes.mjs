e8cd98fc3c01b1b4ef641a170ba98a4feba033a5
camilito2005 <marrugobarrioscamilo2005@gmail.com>
2025-12-15 17:55:24 -0500

    feat: agregue las rutas para el crud

--- Backend/src/Rutas/Horarios.routes.mjs
+++ Backend/src/Rutas/Horarios.routes.mjs
@@ -1,10 +1,14 @@
 import { Router } from "express";
-import { IngresarHorario, ListarHorarios } from "../Controladores/Horarios.controlador.mjs";
+import { AgregarHorario, ListarHorarios,ListarHorariosGuia,Editar,EliminarHorario,Cambiarestado } from "../Controladores/horarios.controlador.mjs";
 import { VerificarToken } from "../Middlewares/Auth.mjs";
 
 const router = Router();
 
-router.post("/agg_horarios",VerificarToken, IngresarHorario);
+router.get('/guia/:id_guia',VerificarToken, ListarHorariosGuia);
+router.post("/agg_horarios",VerificarToken, AgregarHorario);
 router.get("/listar_horarios",VerificarToken, ListarHorarios);
+router.put("/Editar/:id_horario",VerificarToken,Editar);
+router.put("/Cambio/:id_horario",VerificarToken,Cambiarestado);
+router.delete("/Eliminar/:id_horario",VerificarToken,EliminarHorario);
 
 export default router; // exporta las rutas para usarlas en otros archivos
