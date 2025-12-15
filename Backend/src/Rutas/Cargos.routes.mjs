4f73500639eadaa0f36256407d33a3c224030eb0
camilito2005 <marrugobarrioscamilo2005@gmail.com>
2025-12-15 16:42:12 -0500

    feat: añadi las rutas para manejar el crud de los cargos

--- Backend/src/Rutas/Cargos.routes.mjs
+++ Backend/src/Rutas/Cargos.routes.mjs
@@ -1,11 +1,14 @@
 import { Router } from "express";
-import { GetCargos } from "../Controladores/Cargos.controlador.mjs";
-import { Prueba } from "../Controladores/Cargos.controlador.mjs";
+import { GetCargos,Registrar_cargos,Prueba,Editar,Eliminar_cargo } from "../Controladores/Cargos.controlador.mjs";
+import { VerificarToken } from "../Middlewares/Auth.mjs";
 
 const router = Router(); // esta variable nos permite crear rutas
 
 router.get("/", GetCargos);
 router.get("/prueba", Prueba); // ruta de prueba para verificar que las rutas funcionan
+router.post("/Registrar",VerificarToken,Registrar_cargos);
+router.put("/Editar/:id_cargo",VerificarToken,Editar)
+router.delete("/Eliminar/:id_cargo",VerificarToken,Eliminar_cargo);
 // ahora prueba la ruta en el navegador: http://localhost:3000/api/cargos/prueba
 
 
