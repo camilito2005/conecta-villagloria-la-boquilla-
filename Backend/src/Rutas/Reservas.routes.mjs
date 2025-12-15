71a7ed70bfab278aa3c868358e1d1474127ff679
camilito2005 <marrugobarrioscamilo2005@gmail.com>
2025-12-15 17:56:34 -0500

    feat: añadi las rutas para el historial y reservas activas

--- Backend/src/Rutas/Reservas.routes.mjs
+++ Backend/src/Rutas/Reservas.routes.mjs
@@ -2,7 +2,7 @@ import { Router } from "express";
 import { CrearReserva } from "../Controladores/Reservas.Controlador.mjs";
 import { VerificarToken } from "../Middlewares/Auth.mjs"
 import { ObtenerReservasPendientesGuia } from "../Controladores/Reservas.Controlador.mjs";
-import { ObtenerReservasPendientesTurista } from "../Controladores/Reservas.Controlador.mjs";
+import { ObtenerReservasPendientesTurista,ObtenerHistorialReservas } from "../Controladores/Reservas.Controlador.mjs";
 import { ActualizarEstado } from "../Controladores/Reservas.Controlador.mjs";
 
 const router = Router(); // esta variable nos permite crear rutas
@@ -10,8 +10,8 @@ const router = Router(); // esta variable nos permite crear rutas
 router.post("/crear_reserva",VerificarToken, CrearReserva);
 // Ruta para obtener reservas pendientes de un guía específico con el id del guía como query param
 router.get("/pendientes_guia/:id_guia", VerificarToken, ObtenerReservasPendientesGuia);
-router.get("/estado_turista/:id_turista",VerificarToken,ObtenerReservasPendientesTurista);
-// router.put("actualizar_estado/:id_reserva",VerificarToken, ActualizarEstado);
+router.get("/activas/:id_turista",VerificarToken,ObtenerReservasPendientesTurista);
+router.get('/historial/:id_turista',VerificarToken, ObtenerHistorialReservas);
 router.put("/actualizar_estado/:id_reserva",VerificarToken, ActualizarEstado);
 
 
