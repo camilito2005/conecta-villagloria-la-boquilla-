d4876e623ba769d42a47861f7767f9c3b61574e8
camilito2005 <marrugobarrioscamilo2005@gmail.com>
2025-12-20 17:15:40 -0500

    añadi las rutas para crear el crud de las categorias

--- Backend/src/Rutas/Categorias.routes.mjs
+++ Backend/src/Rutas/Categorias.routes.mjs
@@ -1,10 +1,13 @@
 import { Router } from "express";
 import { VerificarToken } from "../Middlewares/Auth.mjs";
-import { ListarCategorias } from "../Controladores/Categorias.Controlador.mjs";
+import { ListarCategorias,CrearCategoria,EditarCategoria,EliminarCategoria } from "../Controladores/Categorias.Controlador.mjs";
 
 const router = Router(); // esta variable nos permite crear rutas
 
 router.get("/", VerificarToken, ListarCategorias);
+router.post("/Crear", VerificarToken, CrearCategoria);
+router.put("/Editar/:id", VerificarToken, EditarCategoria);
+router.delete("/Eliminar/:id", VerificarToken, EliminarCategoria);
 
 
 export default router; // exporta las rutas para usarlas en otros archivos
