0dcb9402da3700b8e3f0bb7bb684459e409eb729
camilito2005 <marrugobarrioscamilo2005@gmail.com>
2026-01-01 19:21:50 -0500

    feat: añadi una condicion para que si no hay sesion no me redirija al login

--- Frontend/src/servicios/Auth.js
+++ Frontend/src/servicios/Auth.js
@@ -3,10 +3,17 @@
 import { useEffect } from "react";
 import { Postdata } from "../servicios/Apis.js";
 
-export function  useVerificarSesion ({ setUsuario, setModalData, navigate }) {
+// voy a agregar un parametro solo para usarlo en el catalogo, nesecito que no me redirija al login 
+
+export function  useVerificarSesion ({ setUsuario, setModalData, navigate, noRedirect = false }) {
   useEffect(() => {
     const verificar = async () => {
       const result = await Postdata("usuarios/verificar", { enviarJson: true });
+    
+      // Si no está autenticado y noRedirect es true, no hacer nada
+      if (noRedirect && result.autenticado === false) {
+        return;
+      }
 
       if (result.showModal && result.modal && result.autenticado === false) {
         setModalData({
