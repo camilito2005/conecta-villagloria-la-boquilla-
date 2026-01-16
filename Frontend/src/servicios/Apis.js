63c69816311fec744d2e23b5f32fefb77bfbfc12
camilito2005 <marrugobarrioscamilo2005@gmail.com>
2026-01-15 23:54:35 -0500

    fix: cambie la ruta base segun la necesidad

--- Frontend/src/servicios/Apis.js
+++ Frontend/src/servicios/Apis.js
@@ -1,4 +1,5 @@
-const BASE_URL = "http://localhost:3000/api/";
+// const BASE_URL = "http://localhost:3000/api/";
+const BASE_URL = `${import.meta.env.VITE_API_URL || "http://localhost:3000"}/api/`;
 // async = siempre devuelve una promesa (Promise) y puedes usar await dentro de ella.
 
 async function Fetchapi(accion, options = {}) {
