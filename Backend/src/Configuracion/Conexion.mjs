4964ebb912255860701df2fcc58aaf1246dffd1d
camilito2005 <marrugobarrioscamilo2005@gmail.com>
2026-01-20 18:41:19 -0500

    refactor: elimine unos comentarios con los que estaba depurando

--- Backend/src/Configuracion/Conexion.mjs
+++ Backend/src/Configuracion/Conexion.mjs
@@ -4,12 +4,6 @@ const { Pool } = pkg;
 import dotenv from "dotenv";
 dotenv.config();
 
-
-console.log("DB_HOST:", process.env.DB_HOST);
-console.log("DB_USER:", process.env.DB_USER);
-console.log("DB_NAME:", process.env.DB_NAME);
-console.log("DB_PORT:", process.env.DB_PORT);
-
 const pool = new Pool({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
