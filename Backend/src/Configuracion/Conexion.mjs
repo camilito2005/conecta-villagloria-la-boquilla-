f9402b85f47d239cae2d0b7185aad5ac8de72602
camilito2005 <marrugobarrioscamilo2005@gmail.com>
2026-01-20 18:21:11 -0500

    fix: corregi los datos para que use los necesarios

--- Backend/src/Configuracion/Conexion.mjs
+++ Backend/src/Configuracion/Conexion.mjs
@@ -4,15 +4,18 @@ const { Pool } = pkg;
 import dotenv from "dotenv";
 dotenv.config();
 
-export const pool = new Pool({
-  host: process.env.DB_HOST,
+
+console.log("DB_HOST:", process.env.DB_HOST);
+console.log("DB_USER:", process.env.DB_USER);
+console.log("DB_NAME:", process.env.DB_NAME);
+console.log("DB_PORT:", process.env.DB_PORT);
+
+const pool = new Pool({
   user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
   database: process.env.DB_NAME,
-  port: Number(process.env.DB_PORT),
-  ssl: {
-    rejectUnauthorized: false,
-  },
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
 });
 
 export default pool;
