3c7f0d5e6e82540db2c1cc4fb8e965bd71751d2e
camilito2005 <marrugobarrioscamilo2005@gmail.com>
2026-01-20 17:43:10 -0500

    fix: corregi el nombre de una variable de entorno

--- Backend/app.mjs
+++ Backend/app.mjs
@@ -42,7 +42,7 @@ app.use(cookieParser());
 //     console.log('Origen bloqueado por CORS:', origin);
 //     return callback(new Error('No permitido por CORS'));
 //   },
-console.log('VITE_NODE_ENV:', process.env.VITE_NODE_ENV);
+console.log('NODE_ENV:', process.env.NODE_ENV);
 app.use(cors({
   origin: process.env.VITE_NODE_ENV === 'production' 
     ? 'https://conecta-villagloria-la-boquilla-frontend.onrender.com'
