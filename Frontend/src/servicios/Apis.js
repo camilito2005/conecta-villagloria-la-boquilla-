8185ce835e65ac99d5a0d1ac9feb09e9bb3a04dd
camilito2005 <marrugobarrioscamilo2005@gmail.com>
2026-01-19 23:09:11 -0500

    fix: adapte opcionalmente a el entorno que sea necesario las rutas del frontend

--- Frontend/src/servicios/Apis.js
+++ Frontend/src/servicios/Apis.js
@@ -1,5 +1,5 @@
 // const BASE_URL = "http://localhost:3000/api/";
-const BASE_URL = import.meta.env.NODE_ENV === "production"
+const BASE_URL = `${import.meta.env.NODE_ENV}` === "production"
   ? "https://conecta-con-villagloria-backend.onrender.com/api/"
   : "http://localhost:3000/api/";
   console.log("BASE_URL:", BASE_URL);
