012dcf1049f6560bf0323a09e3282ae80c19f6dc
camilito2005 <marrugobarrioscamilo2005@gmail.com>
2026-01-20 16:32:13 -0500

    fix: corregi el nombre de una variable de entorno

--- Frontend/src/servicios/Apis.js
+++ Frontend/src/servicios/Apis.js
@@ -1,8 +1,9 @@
 // const BASE_URL = "http://localhost:3000/api/";
-const BASE_URL = `${import.meta.env.NODE_ENV}` === "production"
+const BASE_URL = `${import.meta.env.VITE_NODE_ENV}` === "production"
   ? "https://conecta-con-villagloria-backend.onrender.com/api/"
   : "http://localhost:3000/api/";
   console.log("BASE_URL:", BASE_URL);
+  console.log("entorno:", import.meta.env.VITE_NODE_ENV);
 // const BASE_URL = `${import.meta.env.VITE_URL || "http://localhost:3000"}/api/`;
 // async = siempre devuelve una promesa (Promise) y puedes usar await dentro de ella.
 
