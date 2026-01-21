dfd5bf832fdda68f14ea76ca97742775edfeff2a
camilito2005 <marrugobarrioscamilo2005@gmail.com>
2026-01-20 19:49:24 -0500

    fix: adapte una opcion para que se utilice cuando sea necesario

--- Backend/src/Configuracion/Conexion.mjs
+++ Backend/src/Configuracion/Conexion.mjs
@@ -10,9 +10,8 @@ export const pool = new Pool({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: Number(process.env.DB_PORT),
-  ssl: {
-    rejectUnauthorized: false,
-  },
+  // si el entorno es de producción, habilitar SSL
+  ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false,
 });
 
 export default pool;
