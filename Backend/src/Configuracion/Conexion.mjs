6181fe967053f5d4b09ecf78ab41fe1c8987aa85
camilito2005 <marrugobarrioscamilo2005@gmail.com>
2026-01-20 21:55:16 -0500

    depurando

--- Backend/src/Configuracion/Conexion.mjs
+++ Backend/src/Configuracion/Conexion.mjs
@@ -4,14 +4,17 @@ const { Pool } = pkg;
 import dotenv from "dotenv";
 dotenv.config();
 
-export const pool = new Pool({
+const pool = new Pool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: Number(process.env.DB_PORT),
   // si el entorno es de producción, habilitar SSL
-  ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false,
+  ssl:
+    process.env.NODE_ENV === "production"
+      ? { rejectUnauthorized: false }
+      : false,
 });
 
 export default pool;
