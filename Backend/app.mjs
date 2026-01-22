5dc2a303c4bfb7e73ca564de15b0761d34c3e7a8
camilito2005 <marrugobarrioscamilo2005@gmail.com>
2026-01-22 17:29:28 -0500

    refactor: elimine varios mensajes

--- Backend/app.mjs
+++ Backend/app.mjs
@@ -25,7 +25,6 @@ const __dirname = path.dirname(__filename);
 
 app.use(cookieParser());
 
-console.log('NODE_ENV:', process.env.NODE_ENV);
 app.use(cors({
   origin: process.env.NODE_ENV === 'production' 
     ? 'https://conecta-villagloria-la-boquilla-frontend.onrender.com'
